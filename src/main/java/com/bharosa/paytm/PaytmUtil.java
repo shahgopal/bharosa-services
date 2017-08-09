97fd360248b97bbda1a8756cc3a74192152a00c9
Gopal Shah <gshah@lendingclub.com>
2017-08-08 19:10:10 -0700

    added

--- src/main/java/com/bharosa/paytm/PaytmUtil.java
+++ src/main/java/com/bharosa/paytm/PaytmUtil.java
@@ -17,7 +17,7 @@ public  class PaytmUtil {
     public final static String MERCHANT_KEY="kbzk1DSbJiV_O3p5";
     public final static String INDUSTRY_TYPE_ID="Retail";
     public final static String CHANNEL_ID="WEB";
-    public final static String WEBSITE="worldpressplg";
+    public final static String WEBSITE="DIYtestingweb";
     public final static String PAYTM_URL="https://pguat.paytm.com/oltp-web/processTransaction";
 
 
