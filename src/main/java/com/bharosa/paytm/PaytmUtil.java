d9a70a9747fb10225099529f537ae6af07c440f7
Gopal Shah <gshah@lendingclub.com>
2017-08-08 19:18:25 -0700

    added

--- src/main/java/com/bharosa/paytm/PaytmUtil.java
+++ src/main/java/com/bharosa/paytm/PaytmUtil.java
@@ -13,12 +13,14 @@ import com.paytm.pg.merchant.CheckSumServiceHelper;
 
 public  class PaytmUtil {
 
-    public final static String MID="WorldP64425807474247";
+    public final static String MID="DIY12386817555501617";
     public final static String MERCHANT_KEY="kbzk1DSbJiV_O3p5";
     public final static String INDUSTRY_TYPE_ID="Retail";
     public final static String CHANNEL_ID="WEB";
     public final static String WEBSITE="DIYtestingweb";
     public final static String PAYTM_URL="https://pguat.paytm.com/oltp-web/processTransaction";
+
+    
 
 
     public static TreeMap<String, String> generatePayLoad(PaytmRequestModel paytmRequestModel) {
