0c7a0a7bdf5ddf9cac933012e170b245504e6126
Gopal Shah <gshah@lendingclub.com>
2017-08-18 18:04:29 -0700

    Commeting security

--- src/main/java/com/bharosa/SecurityConfiguration.java
+++ src/main/java/com/bharosa/SecurityConfiguration.java
@@ -1,32 +1,33 @@
 package com.bharosa;
 
-import org.springframework.boot.autoconfigure.security.SecurityProperties;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.core.annotation.Order;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.web.savedrequest.NullRequestCache;
+//import org.springframework.boot.autoconfigure.security.SecurityProperties;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.core.annotation.Order;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+//import org.springframework.security.config.http.SessionCreationPolicy;
+//import org.springframework.security.web.savedrequest.NullRequestCache;
 
-@Configuration
-@Order(SecurityProperties.ACCESS_OVERRIDE_ORDER)
-public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+//@Configuration
+//@Order(SecurityProperties.ACCESS_OVERRIDE_ORDER)
+public class SecurityConfiguration 
+//extends WebSecurityConfigurerAdapter 
+{
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http
-                .authorizeRequests()
-                .antMatchers("/api/session").permitAll()
-                .antMatchers("/h2-console/**").permitAll()
-                .antMatchers("/api/**").authenticated()
+//    @Override
+//    protected void configure(HttpSecurity http) throws Exception {
+//        http
+//                .authorizeRequests()
+//                .antMatchers("/api/session").permitAll()
+//                .antMatchers("/h2-console/**").permitAll()
 //                .antMatchers("/api/**").authenticated()
-                .and()
-                .headers().frameOptions().disable() // for h2
-                .and()
-                .requestCache()
-                .requestCache(new NullRequestCache())
-                .and()
-                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED);
+//                .and()
+//                .headers().frameOptions().disable() // for h2
+//                .and()
+//                .requestCache()
+//                .requestCache(new NullRequestCache())
+//                .and()
+//                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED)
 //                .and().csrf().disable();
-    }
+//    }
 }
