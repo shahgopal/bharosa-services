6fd504e9917236d6ea59988d7b5cd146d0fa13e8
Gopal Shah <gshah@lendingclub.com>
2017-12-03 18:13:27 -0800

    If user is not signed

---
+++ src/main/java/com/bharosa/api/UnsignedPaymentRequestControllar.java
@@ -0,0 +1,106 @@
+package com.bharosa.api;
+
+import java.util.Map;
+import java.util.TreeMap;
+import java.util.UUID;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.bharosa.model.Campaign;
+import com.bharosa.model.PaymentRequest;
+import com.bharosa.model.PaytmRequestModel;
+import com.bharosa.paytm.PaytmUtil;
+import com.bharosa.repository.CampaignRepository;
+import com.bharosa.repository.PaymentRequestRepository;
+
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+
+@RestController
+@RequestMapping("/anymspayres")
+@Api(value = "Payment api" )
+public class UnsignedPaymentRequestControllar {
+
+	
+	@Autowired
+	PaymentRequestRepository paymentRequestRepository;
+	@Autowired
+	CampaignRepository campaignRepository;
+	
+	
+	@RequestMapping(value = "/paymentrequests", method = RequestMethod.GET)
+	@CrossOrigin
+	public ResponseEntity<Iterable<PaymentRequest>> getPaymentRequests() {
+		return new ResponseEntity<>(paymentRequestRepository.findAll(), HttpStatus.OK);
+	}
+	
+	@RequestMapping(value = "/paymentrequest/{id}", method = RequestMethod.GET)
+	@CrossOrigin
+	public ResponseEntity<PaymentRequest> getPaymentRequest(@PathVariable long id) {
+		PaymentRequest paymentRequest = paymentRequestRepository.findOne(id);
+		if (paymentRequest != null) {
+			return new ResponseEntity<>(paymentRequest, HttpStatus.OK);
+		} else {
+			return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
+		}
+	}
+
+	@ApiOperation(value = "Do not use", notes = "do not use")
+	@RequestMapping(value = "/paymentrequest", method = RequestMethod.POST)
+	@CrossOrigin
+	public ResponseEntity<PaymentRequest> createPaymentRequest(@RequestBody PaymentRequest paymentRequest) {
+		System.out.println("paymentRequest.getCampaign().getId() is " + paymentRequest.getCampaign().getId());
+		
+		if(paymentRequest.getCampaign() != null && paymentRequest.getCampaign().getId() > 0){
+			paymentRequest.setCampaign(campaignRepository.findOne(paymentRequest.getCampaign().getId()));
+		}
+		PaymentRequest savedPR = paymentRequestRepository.save(paymentRequest);
+		return new ResponseEntity<>(savedPR, HttpStatus.OK);
+	}
+
+
+	@ApiOperation(value = "Provide paytm request", notes = "Returns checksum and other parameters")
+	@RequestMapping(value = "/requestpaytm", method = RequestMethod.POST, consumes = "application/json")
+	@CrossOrigin
+	public ResponseEntity<TreeMap<String, String>> createPaytmLoad(@RequestBody PaytmRequestModel paytmRequestModel) {
+
+		
+		TreeMap<String, String> paramMap = null;
+		System.out.println("System.out.println(paytmRequestModel.getCAMPAIGN_ID());"+paytmRequestModel.getCAMPAIGN_ID());
+		if (paytmRequestModel.getCAMPAIGN_ID() != 0) {
+			Campaign campaign = campaignRepository.findOne(paytmRequestModel.getCAMPAIGN_ID());
+			System.out.println("Campaign"+campaign.getDetails());
+//			paytmRequestModel.getORDER_ID().toString() 
+//			paymentRequestRepository.save(pr);
+			paramMap = PaytmUtil.generatePayLoad(paytmRequestModel);
+			PaymentRequest pr = PaytmUtil.paymentRequestBuilder(paytmRequestModel);
+			pr.setCampaign(campaign);
+			pr.setChecksumHash(paramMap.get("CHECKSUMHASH"));
+			paymentRequestRepository.save(pr);
+			return new ResponseEntity<>(paramMap, HttpStatus.OK);
+		}	
+		else {
+			return new ResponseEntity<>(paramMap, HttpStatus.FAILED_DEPENDENCY);	
+		}
+
+		
+//		if(paytmRequestModel.getCAMPAIGN_ID() == 0 || paymentRequestRepository.find > 0){
+//			paymentRequest.setCampaign(campaignRepository.findOne(campaignRepository.fin));
+//		}
+//		TreeMap<String, String> paramMap = PaytmUtil.generatePayLoad(paytmRequestModel);
+//		PaymentRequest pr = PaytmUtil.paymentRequestBuilder(paytmRequestModel);
+//		pr.setCampaign(campaignRepository.findOne(paytmRequestModel.getCAMPAIGN_ID()));
+//		paymentRequestRepository.save(pr);
+//		return new ResponseEntity<>(paramMap, HttpStatus.OK);
+	}
+
+	
+}
---
+++ src/main/java/com/bharosa/api/UnsignedPaymentResponseControllar.java
@@ -0,0 +1,137 @@
+package com.bharosa.api;
+
+import java.math.BigDecimal;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.TreeMap;
+import java.util.UUID;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.bharosa.model.Campaign;
+import com.bharosa.model.PaymentRequest;
+import com.bharosa.model.PaymentResponse;
+import com.bharosa.model.PaytmRequestModel;
+import com.bharosa.model.PaytmResponseModel;
+import com.bharosa.paytm.PaytmUtil;
+import com.bharosa.repository.CampaignRepository;
+import com.bharosa.repository.PaymentRequestRepository;
+import com.bharosa.repository.PaymentResponseRepository;
+
+import io.swagger.annotations.ApiOperation;
+import org.springframework.util.MultiValueMap;
+@RestController
+@RequestMapping("/anymspayreq")
+public class UnsignedPaymentResponseControllar {
+
+	
+	@Autowired
+	PaymentResponseRepository paymentResponseRepository;
+	@Autowired
+	CampaignRepository campaignRepository;
+	@Autowired
+	PaymentRequestRepository paymentRequestRepository;
+
+	
+	@RequestMapping(value = "/paymentresponses", method = RequestMethod.GET)
+	@CrossOrigin
+	public ResponseEntity<Iterable<PaymentResponse>> getPaymentResponses() {
+		return new ResponseEntity<>(paymentResponseRepository.findAll(), HttpStatus.OK);
+	}
+	
+	@RequestMapping(value = "/paymentresponse/{id}", method = RequestMethod.GET)
+	@CrossOrigin
+	public ResponseEntity<PaymentResponse> getPaymentResponse(@PathVariable long id) {
+		PaymentResponse paymentResponse = paymentResponseRepository.findOne(id);
+		if (paymentResponse != null) {
+			return new ResponseEntity<>(paymentResponse, HttpStatus.OK);
+		} else {
+			return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
+		}
+	}
+
+	@RequestMapping(value = "/paymentresponse", method = RequestMethod.POST)
+	@CrossOrigin
+	public ResponseEntity<PaymentResponse> createPaymentResponse(@RequestBody PaymentResponse paymentResponse) {
+		
+		if(paymentResponse.getCampaign() != null && paymentResponse.getCampaign().getId() > 0)
+			paymentResponse.setCampaign(campaignRepository.findOne(paymentResponse.getCampaign().getId()));
+			
+		PaymentResponse savedPR = paymentResponseRepository.save(paymentResponse);
+		return new ResponseEntity<>(savedPR, HttpStatus.OK);
+	}
+
+	@ApiOperation(value = "Process paytm response", notes = "Returns success or failure")
+	@RequestMapping(value = "/paytmresponse", method = RequestMethod.POST, consumes = "application/x-www-form-urlencoded")
+	@CrossOrigin
+//	public ResponseEntity<PaymentResponse> processPaymentResponse(@RequestBody PaytmResponseModel paytmResponseModel) {
+	public ResponseEntity<String> processPaymentResponse(@RequestBody MultiValueMap<String, Object> bodyMap) {
+		
+		PaytmResponseModel paytmResponseModel = new PaytmResponseModel();
+		System.out.println("MultiValueMap"+bodyMap.toString());
+
+		paytmResponseModel.setMID((String)bodyMap.getFirst("MID"));
+		paytmResponseModel.setTXNID((String)bodyMap.getFirst("TXNID")); 
+		paytmResponseModel.setORDERID((UUID.fromString((String)bodyMap.getFirst("ORDERID")))); 
+		paytmResponseModel.setBANKTXNID((String)bodyMap.getFirst("BANKTXNID")); 
+		paytmResponseModel.setTXNAMOUNT((String)bodyMap.getFirst("TXNAMOUNT")); 
+		paytmResponseModel.setCURRENCY((String)bodyMap.getFirst("CURRENCY"));
+		paytmResponseModel.setSTATUS((String)bodyMap.getFirst("STATUS")); 
+		paytmResponseModel.setRESPCODE((String)bodyMap.getFirst("RESPCODE"));
+		paytmResponseModel.setRESPMSG((String)bodyMap.getFirst("RESPMSG"));
+		
+		SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS");
+	
+		try {
+			paytmResponseModel.setTXNDATE(formatter.parse((String)bodyMap.getFirst("TXNDATE")));
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			System.out.println("Cudnt set the date");
+		}
+		
+		
+		paytmResponseModel.setGATEWAYNAME((String)bodyMap.getFirst("GATEWAYNAME")); 
+		paytmResponseModel.setBANKNAME((String)bodyMap.getFirst("BANKNAME"));
+		paytmResponseModel.setPAYMENTMODE((String)bodyMap.getFirst("PAYMENTMODE"));
+		paytmResponseModel.setCHECKSUMHASH((String)bodyMap.getFirst("CHECKSUMHASH"));
+		paytmResponseModel.setTXNTYPE((String)bodyMap.getFirst("TXNTYPE"));
+		
+		PaymentResponse pr = PaytmUtil.paymentResponseBuilder(paytmResponseModel);
+		System.out.println("paytmResponseModel"+paytmResponseModel.toString());
+		System.out.println("paytmResponseModel"+ paytmResponseModel.getORDERID());
+		System.out.println("paytmResponseModel"+ pr.getOrderId());
+		PaymentRequest paymentRequest = paymentRequestRepository.findByOrderId(pr.getOrderId());
+		pr.setCampaign(paymentRequest.getCampaign());
+		pr.setPaymentRequest(paymentRequest);
+		pr.setIsValidchecksumHash(PaytmUtil.isValidChecksum(paytmResponseModel, paymentRequest));
+		
+		PaymentResponse savedPR = paymentResponseRepository.save(pr);
+//		pr.setCampaign(campaignRepository.findOne(paytmRequestModel.getCAMPAIGN_ID()));
+		
+		if (savedPR.getStatus().equalsIgnoreCase("TXN_SUCCESS")) {
+			return new ResponseEntity<>(
+					"Thank you for your Donation! We were able to process the transaction successfully", HttpStatus.OK);
+		} else if (savedPR.getStatus().equalsIgnoreCase("PENDING")) {
+			return new ResponseEntity<>("Thank you for your Donation! Your transaction is in pending status ",
+					HttpStatus.OK);
+		}
+//		else (savedPR.getStatus().equalsIgnoreCase("TXN_FAILURE")) {
+		else{
+			return new ResponseEntity<>("We were unable to process the transaction Please try again", HttpStatus.OK);
+		}			
+		
+		
+	}
+
+	
+}
